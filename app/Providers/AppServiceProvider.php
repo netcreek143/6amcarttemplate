922f22ac18b8b6dcda6f25d1ac7ef84a9ca26d9a
netcreek143 <sekarsuriya16@gmail.com>
2026-02-06 21:05:54 +0530

    Debug: Print DB config in AppServiceProvider to debug startup crash

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,19 +28,18 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        error_log('🔍 DEBUG: APP_BOOT: DB_HOST=' . config('database.connections.mysql.host'));
+        error_log('🔍 DEBUG: APP_BOOT: DB_PORT=' . config('database.connections.mysql.port'));
+        error_log('🔍 DEBUG: APP_BOOT: DB_DATABASE=' . config('database.connections.mysql.database'));
 
-        try
-        {
-            Config::set('addon_admin_routes',$this->get_addon_admin_routes());
-            Config::set('get_payment_publish_status',$this->get_payment_publish_status());
+        try {
+            Config::set('addon_admin_routes', $this->get_addon_admin_routes());
+            Config::set('get_payment_publish_status', $this->get_payment_publish_status());
             Paginator::useBootstrap();
-            foreach(Helpers::get_view_keys() as $key=>$value)
-            {
+            foreach (Helpers::get_view_keys() as $key => $value) {
                 view()->share($key, $value);
             }
-        }
-        catch(\Exception $e)
-        {
+        } catch (\Exception $e) {
 
         }
 
